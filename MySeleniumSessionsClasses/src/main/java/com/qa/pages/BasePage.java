f5a1e21bd47d8cb3ccd675bd4064b9bc10141b66
Aviet Lobo <avietlobo@gmail.com>
2020-01-18 11:48:25 +0530

    updated

--- MySeleniumSessionsClasses/src/main/java/com/qa/pages/BasePage.java
+++ MySeleniumSessionsClasses/src/main/java/com/qa/pages/BasePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+
 
 public class BasePage {
 	
